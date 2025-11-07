f3b6c9504c5ec969af73943379dbefef32c4a8af
/kill <90850818+Slash-Kill@users.noreply.github.com>
2025-11-07 16:38:28 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10156751
+    "zoneId": 10156817
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
